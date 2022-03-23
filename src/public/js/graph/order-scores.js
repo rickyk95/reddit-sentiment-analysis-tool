17f62a95c62b09a74675a1fd278de2c236d61d20
Btilly <ricky@Rikys-MacBook-Pro.local>
2022-03-22 23:14:39 -0600

    new commit typo fix

--- src/public/js/graph/order-scores.js
+++ src/public/js/graph/order-scores.js
@@ -1,16 +1,11 @@
-
-
-
 let keys = [];
 let scores = [];
 function orderScores(array){
-
 
     for(var i = 0; i < array.length;i++){
         keys.push(i)  
         scores.push(array[i].score)  
     }
-
 }
 
 
